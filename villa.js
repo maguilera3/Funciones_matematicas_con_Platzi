b2c669fb11b938d56cb152772759dd65a012ef66
Maye <mayerlingnas@gmail.com>
2019-05-15 08:39:58 -0400

    Funcion Matematic JavaScript
    Math.floor(x); ---> Redondeo hacia abajo
    Math.ceil(y) ; ----> Redondeo hacia arriba
    Math.randow; ----> Numero Randon de 0.00001 a  0.999999

--- villa.js
+++ villa.js
@@ -1,2 +1,10 @@
+var z = aletorio(10, 20);
+document.write(z);
 
-console.log("HOLA");
+function aleatorio(min, max)
+{
+	var reul;
+	
+	resul = Math.floor(Math.randow() * (maxi - min + 1) +min);
+	return resul;
+}
